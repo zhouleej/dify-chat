a14a1187b482ff3d29cd91f058e83785bafc7521
lexmin0412 <zhangle_dev@outlook.com>
2025-03-01 20:42:16 +0800

    feat: 添加 Github 跳转链接

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,13 +1,23 @@
+import { GithubOutlined } from "@ant-design/icons";
+import { Button } from "antd";
+
 export const Logo = () => (
   <div className="flex h-16 items-center justify-start py-0 px-6 box-border">
-    <img
-      className="w-6 h-6 inline-block"
-      src="https://mdn.alipayobjects.com/huamei_iwk9zp/afts/img/A*eco6RrQhxbMAAAAAAAAAAAAADgCCAQ/original"
-      draggable={false}
-      alt="logo"
-    />
-    <span className="inline-block my-0 mx-2 font-bold text-gray-700 text-lg">
-      Dify Chat
-    </span>
+    <div className="h-full flex items-center flex-1 overflow-hidden">
+      <img
+        className="w-6 h-6 inline-block"
+        src="https://mdn.alipayobjects.com/huamei_iwk9zp/afts/img/A*eco6RrQhxbMAAAAAAAAAAAAADgCCAQ/original"
+        draggable={false}
+        alt="logo"
+      />
+      <span className="inline-block my-0 mx-2 font-bold text-gray-700 text-lg">
+        Dify Chat
+      </span>
+    </div>
+		<div>
+			<Button type="link" href="https://github.com/lexmin0412/dify-chat" target="_blank" className="px-0">
+				<GithubOutlined className="text-lg cursor-pointer text-[#333]" />
+			</Button>
+		</div>
   </div>
 );
