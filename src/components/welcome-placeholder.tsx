f1870f43690d84e3e87deb85699f3cb1e458bf54
lexmin0412 <zhangle_dev@outlook.com>
2025-03-02 07:11:08 +0800

    refactor: 更新欢迎提示描述文字

--- src/components/welcome-placeholder.tsx
+++ src/components/welcome-placeholder.tsx
@@ -84,8 +84,8 @@ export const WelcomePlaceholder = (props: IWelcomePlaceholderProps) => {
       <Welcome
         variant="borderless"
         icon="https://mdn.alipayobjects.com/huamei_iwk9zp/afts/img/A*s5sNRo5LjfQAAAAAAAAAAAAADgCCAQ/fmt.webp"
-        title="Hello, I'm Ant Design X"
-        description="Base on Ant Design, AGI product interface solution, create a better intelligent vision~"
+        title="Hello, I'm Dify Chat"
+        description="Base on Dify API, Dify Chat is a web app that can interact with AI."
         extra={
           <Space>
             <Button icon={<ShareAltOutlined />} />
