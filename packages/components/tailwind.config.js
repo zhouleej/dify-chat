1c86bf0ed82793e98f566bb0e96f4b4506ff9e61
lexmin0412 <zhangle_dev@outlook.com>
2025-03-13 17:33:28 +0800

    chore(components): theme.extend.colors 与主项目同步

--- packages/components/tailwind.config.js
+++ packages/components/tailwind.config.js
@@ -1,3 +1,12 @@
+/** @type {import('tailwindcss').Config.theme.extend.colors} */
+const colors = {
+  default: '#333',
+	desc: '#9CA3B3',
+  eb: '#ebebeb',
+  warning: '#FF5A07',
+	primary: '#4C84FF'
+}
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -5,7 +14,9 @@ module.exports = {
     './stories/*.{html,js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors
+    },
   },
   plugins: [],
 };
