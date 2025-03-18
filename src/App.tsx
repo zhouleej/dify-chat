6f8e84a1b25d460b8adadeaa120639e385bbcee4
lexmin0412 <zhangle_dev@outlook.com>
2025-03-18 22:52:14 +0800

    feat(路由): add pure-react-router, ready for mobile route

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import { USER } from "./config";
 import { DifyChatProvider } from "@dify-chat/core";
-import DifyChatWrapper from "./wrapper";
+import { BrowserRouter, Route, type IRoute } from "pure-react-router";
+import ChatPage from "./pages/chat";
+import AppListPage from "./pages/app-list";
+
+const routes: IRoute[] = [
+  { path: "/chat", component: ()  => <ChatPage /> },
+  { path: "/apps", component: () => <AppListPage /> },
+  { path: "/", component: ()  => <ChatPage /> },
+];
 
 /**
  * Dify Chat 的最小应用实例
  */
 export default function App() {
   return (
-    <DifyChatProvider value={{user: USER}}>
-      <DifyChatWrapper />
-    </DifyChatProvider>
+    <BrowserRouter basename="/dify-chat" routes={routes}>
+      <DifyChatProvider value={{user: USER}}>
+        <Route />
+      </DifyChatProvider>
+    </BrowserRouter>
   )
 }
---
+++ src/pages/chat/index.tsx
@@ -0,0 +1,5 @@
+import DifyChatWrapper from "../../wrapper";
+
+export default function ChatPage() {
+    return <DifyChatWrapper />
+}
