68f2f14ea6243daf479d21afe06c050f4fd444d0
lexmin0412 <zhangle_dev@outlook.com>
2025-03-18 10:32:50 +0800

    refactor: 移除无用的 getVars 方法

--- packages/helpers/src/vars.ts
+++ packages/helpers/src/vars.ts
@@ -1,17 +1,3 @@
-export const RUNTIME_VARS_KEY = 'DIFY_CHAT__RUNTIME_VARS'
-
-/**
- * 获取变量
- */
-export const getVars = () => {
-	const runtimeVars = JSON.parse(localStorage.getItem(RUNTIME_VARS_KEY) || '{}')
-	return {
-		DIFY_API_BASE: runtimeVars.DIFY_API_BASE || process.env.DIFY_API_BASE,
-		DIFY_API_KEY: runtimeVars.DIFY_API_KEY || process.env.DIFY_API_KEY,
-		DIFY_API_VERSION: runtimeVars.DIFY_API_VERSION || process.env.DIFY_API_VERSION,
-	}
-}
-
 const DIFY_VERSION_KEY = 'DIFY_CHAT__DIFY_VERSION'
 
 /**
