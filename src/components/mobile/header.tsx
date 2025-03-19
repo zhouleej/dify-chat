97e39a0fcc0417f8a443682e16fb23ec4172e75b
lexmin0412 <zhangle_dev@outlook.com>
2025-03-19 11:48:04 +0800

    feat(components): 添加移动端公用头部组件
    新增 `MobileHeader` 组件，用于移动端页面的头部布局。该组件支持自定义中间内容，并包含 Logo 和 Github 图标链接。

---
+++ src/components/mobile/header.tsx
@@ -0,0 +1,30 @@
+import { Link } from "pure-react-router"
+import { GithubIcon, LogoIcon } from "../logo"
+
+interface IMobileHeaderProps {
+  centerChildren: JSX.Element
+}
+
+/**
+ * 移动端共用头部
+ */
+export const MobileHeader = (props: IMobileHeaderProps) => {
+
+  const { centerChildren } = props
+
+  return (
+    <div
+        className='h-12 !leading-[3rem] px-4 text-base top-0 z-20 bg-white w-full shadow-sm font-semibold justify-between flex items-center box-border'
+      >
+        <Link to='/apps' className='flex items-center'>
+          <LogoIcon />
+        </Link>
+
+        <div className="flex-1 overflow-hidden flex items-center justify-center">
+          {centerChildren}
+        </div>
+
+        <GithubIcon />
+      </div>
+  )
+}
