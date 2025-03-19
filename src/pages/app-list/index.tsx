fd8fbcec7c95aea41316b4248b9d41a0f35d37b2
lexmin0412 <zhangle_dev@outlook.com>
2025-03-19 14:05:08 +0800

    style: 统一代码格式并修复标题显示
    修改页面标题(“Dify Chat”=>“应用列表”)以更准确地反映页面内容

--- src/pages/app-list/index.tsx
+++ src/pages/app-list/index.tsx
@@ -1,64 +1,59 @@
-import { Tag } from "antd";
-import { useEffect, useState } from "react";
-import { IDifyAppItem } from "@dify-chat/core";
-import DifyAppLocalStorageStore from "../../storage/app";
-import { useHistory } from "pure-react-router";
-import { MobileHeader } from "../../components/mobile/header";
+import { Tag } from 'antd';
+import { useEffect, useState } from 'react';
+import { IDifyAppItem } from '@dify-chat/core';
+import DifyAppLocalStorageStore from '../../storage/app';
+import { useHistory } from 'pure-react-router';
+import { MobileHeader } from '../../components/mobile/header';
 
-const appStore = new DifyAppLocalStorageStore()
+const appStore = new DifyAppLocalStorageStore();
 
 export default function AppListPage() {
+  const history = useHistory();
+  const [list, setList] = useState<IDifyAppItem[]>([]);
 
-	const history = useHistory()
-	const [list, setList] = useState<IDifyAppItem[]>([])
+  const getAppList = async () => {
+    const result = await appStore.getApps();
+    setList(result);
+  };
 
-	const getAppList = async () => {
-		const result = await appStore.getApps()
-		setList(result)
-	}
+  useEffect(() => {
+    getAppList();
+  }, []);
 
-	useEffect(() => {
-		getAppList()
-	}, [])
-
-	return (
-		<div>
-			<MobileHeader centerChildren={<>Dify Chat</>} />
-			<div className="px-3">
-			{
-				list.map((item) => {
-					return (
-						<div key={item.id} className={`p-3 bg-white mt-3 border border-solid border-gray-200 rounded-lg cursor-pointer hover:border-primary hover:text-primary`}
-							onClick={() => {
-								history.push(`/chat?id=${item.id}`)
-							}}
-						>
-							<div className="w-full flex items-center">
-								<div className="flex-1 overflow-hidden flex items-center">
-									<span className="font-semibold">
-										{item.info.name}
-									</span>
-									{
-										item.info.tags ?
-											item.info.tags.map((tag) => {
-												return (
-													<Tag key={tag} className="ml-2">
-														{tag}
-													</Tag>
-												)
-											})
-											: null
-									}
-								</div>
-							</div>
-							<div className="truncate text-sm mt-2">
-								{item.info.description}
-							</div>
-						</div>
-					)
-				})
-			}
-		</div>
-		</div>
-	)
+  return (
+    <div>
+      <MobileHeader centerChildren={<>应用列表</>} />
+      <div className="px-3">
+        {list.map((item) => {
+          return (
+            <div
+              key={item.id}
+              className={`p-3 bg-white mt-3 border border-solid border-gray-200 rounded-lg cursor-pointer hover:border-primary hover:text-primary`}
+              onClick={() => {
+                history.push(`/chat?id=${item.id}`);
+              }}
+            >
+              <div className="w-full flex items-center">
+                <div className="flex-1 overflow-hidden flex items-center">
+                  <span className="font-semibold">{item.info.name}</span>
+                  {item.info.tags
+                    ? item.info.tags.map((tag) => {
+                        return (
+                          <Tag key={tag} className="ml-2">
+                            {tag}
+                          </Tag>
+                        );
+                      })
+                    : null}
+                </div>
+              </div>
+              <div className="truncate text-sm mt-2">
+                {item.info.description}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
