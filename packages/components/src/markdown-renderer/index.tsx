0b5cf9f832213afa32bb0fa2655cdbc1c4a6c1ac
lexmin0412 <zhangle_dev@outlook.com>
2025-03-24 16:41:53 +0800

    feat(markdown-renderer): 添加 markdown-it-katex-gpt 插件以支持数学公式渲染

--- packages/components/src/markdown-renderer/index.tsx
+++ packages/components/src/markdown-renderer/index.tsx
@@ -1,30 +1,43 @@
-import { Typography } from "antd"
+import { Typography } from 'antd';
 import MarkdownIt from 'markdown-it';
-import MarkdownItPluginEcharts from '@lexmin0412/markdown-it-echarts'
-import { useMemo } from "react";
+import MarkdownItPluginEcharts from '@lexmin0412/markdown-it-echarts';
+// @ts-expect-error 此库未提供类型定义
+import markdownItPluginKatex from 'markdown-it-katex-gpt';
+import { useMemo } from 'react';
 
 interface IMarkdownRendererProps {
   /**
    * 原始 Markdown 文本
    */
-  markdownText: string
+  markdownText: string;
 }
 
-const md = MarkdownIt({ html: true, breaks: true }).use(MarkdownItPluginEcharts)
+const md = MarkdownIt({ html: true, breaks: true })
+  .use(MarkdownItPluginEcharts)
+  .use(markdownItPluginKatex, {
+    delimiters: [
+      { left: '\\[', right: '\\]', display: true },
+      { left: '\\(', right: '\\)', display: false },
+      { left: '$$', right: '$$', display: false },
+    ]
+  });
 
 /**
  * Markdown 渲染组件
  */
 export const MarkdownRenderer = (props: IMarkdownRendererProps) => {
-  const { markdownText } = props
+  const { markdownText } = props;
 
-  const dangerousHTML = useMemo(()=>{
-    return md.render(markdownText)
-  }, [markdownText])
+  const dangerousHTML = useMemo(() => {
+    return md.render(markdownText);
+  }, [markdownText]);
 
   return (
     <Typography>
-      <div className="w-full overflow-hidden" dangerouslySetInnerHTML={{ __html: dangerousHTML }} />
+      <div
+        className="w-full overflow-hidden"
+        dangerouslySetInnerHTML={{ __html: dangerousHTML }}
+      />
     </Typography>
-  )
+  );
 };
