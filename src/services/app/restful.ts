d10652fbb0318004cf809ababe3bd6a6f74fc45d
lexmin0412 <zhangle_dev@outlook.com>
2025-03-25 21:54:46 +0800

    feat(services): 新增 restful 的应用服务实现
    引入 `DifyAppService` 类，分别通过 RESTful API 和 localStorage 实现应用列表的 CRUD 操作。同时，添加 `BaseRequest` 类用于处理 HTTP 请求，提供 GET、POST、PUT 和 DELETE 方法的基础支持。

---
+++ src/services/app/restful.ts
@@ -0,0 +1,39 @@
+import { DifyAppStore, type IDifyAppItem } from '@dify-chat/core';
+import { BaseRequest } from '@/services/base-request';
+
+const API_BASE_URL = 'http://localhost:3000';
+
+const request = new BaseRequest({baseURL: API_BASE_URL});
+
+/**
+ * 应用列表 CRUD 的 RESTful 实现
+ */
+class DifyAppService extends DifyAppStore {
+  async getApps(): Promise<IDifyAppItem[]> {
+    const response = await request.get(`/apps`);
+    return response;
+  }
+
+  async getApp(id: string): Promise<IDifyAppItem | undefined> {
+    try {
+      const response = await request.get(`/apps/${id}`);
+      return response;
+    } catch (error) {
+      return undefined;
+    }
+  }
+
+  async addApp(config: IDifyAppItem): Promise<void> {
+    await request.post(`/apps`, config as unknown as Record<string, unknown>);
+  }
+
+  async updateApp(config: IDifyAppItem): Promise<void> {
+    return request.put(`/apps/${config.id}`, config as unknown as Record<string, unknown>);
+  }
+
+  async deleteApp(id: string): Promise<void> {
+    await request.delete(`/apps/${id}`);
+  }
+}
+
+export default DifyAppService;
---
+++ src/services/base-request.ts
@@ -0,0 +1,69 @@
+export class BaseRequest {
+
+  constructor(options: {
+    baseURL: string,
+  }) {
+    this.options = options
+  }
+
+  options: {
+    baseURL: string,
+  }
+
+  async baseRequest(url: string, options: RequestInit) {
+    const result = await fetch(`${this.options.baseURL}${url}`, {
+      ...options,
+    })
+    return result
+  }
+
+  async jsonRequest(url: string, options: RequestInit) {
+    const result = await this.baseRequest(url, {
+      ...options,
+      headers: {
+        ...options.headers,
+        'Content-Type': 'application/json',
+      }
+    })
+    return result.json()
+  }
+
+  async get(url: string, params: Record<string, string> = {}, headers: Record<string, string> = {}) {
+    const queryString = params && Object.keys(params).length > 0 ? `?${new URLSearchParams(params).toString()}` : ''
+    const result = await this.jsonRequest(`${url}${queryString}`, {
+      method: 'GET',
+      headers
+    })
+    return result
+  }
+
+  async post(url: string, params: Record<string, unknown> = {}, headers: Record<string, string> = {}) {
+    const result = await this.jsonRequest(url, {
+      method: 'POST',
+      body: JSON.stringify(params),
+      headers
+    })
+    return result
+  }
+
+	async put(url: string, params: Record<string, unknown> = {}, headers: Record<string, string> = {}) {
+		const result = await this.jsonRequest(url, {
+			method: 'PUT',
+			body: JSON.stringify(params),
+			headers
+		})
+		return result
+	}
+
+	async delete(url: string, params: Record<string, unknown> = {}, headers: Record<string, string> = {}) {
+		const result = await this.baseRequest(url, {
+			method: 'DELETE',
+			body: JSON.stringify(params),
+			headers: {
+        ...headers,
+        'Content-Type': 'application/json',
+      }
+		})
+		return result
+	}
+}
