12b0b62ae856f74fb5d43213580d4d646d73bdc9
lexmin0412 <zhangle_dev@outlook.com>
2025-03-25 21:42:41 +0800

    feat(hooks): 在 use-dify-chat 中添加 appService 属性, 便于使用方自定义应用管理的后端实现

--- packages/core/src/hooks/use-dify-chat.ts
+++ packages/core/src/hooks/use-dify-chat.ts
@@ -1,14 +1,19 @@
 import React, { useContext } from "react";
+import { DifyAppStore } from "../storage";
 
 interface IDifyChatContext {
   /**
    * 用户 ID, 用于传递给 dify API 的用户唯一标识
    */
   user: string
+  /**
+   * App 服务，用于实现应用列表的 CRUD 管理
+   */
+  appService?: DifyAppStore
 }
 
 const DEFAULT_CONTEXT_VALUE: IDifyChatContext = {
-  user: ''
+  user: '',
 }
 
 /**
