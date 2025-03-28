d7b3f136ff4002aee0c9cf5a9ea51502fa3334ea
lexmin0412 <zhangle_dev@outlook.com>
2025-03-28 23:51:15 +0800

    refactor(tailwind.config): 移除开发环境判断，始终包含 packages/components 目录
    移除对 `process.env.NODE_ENV` 的判断，使 `packages/components` 目录的内容在所有环境下都被包含。这简化了配置并确保一致性。

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,10 +11,8 @@ const minWidth = {
 
 const content = ['./src/**/*.{html,js,ts,jsx,tsx}']
 
-// 如果是开发环境，则添加 packages/components 目录
-if (process.env.NODE_ENV === 'development') {
-	content.push('./packages/components/src/**/*.{html,js,ts,jsx,tsx}')
-}
+// 则添加 packages/components 目录
+content.push('./packages/components/src/**/*.{html,js,ts,jsx,tsx}')
 
 const config: Config = {
 	content,
