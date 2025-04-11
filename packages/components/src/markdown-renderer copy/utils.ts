fa803b542e733e060d1742f3132c6a40fc6ab243
lexmin0412 <zhangle_dev@outlook.com>
2025-04-11 03:59:27 +0000

    feat: 支持 mermaid 渲染, 使用 react-markdown 替换 markdown-it 以提升后续可维护性(markdown-it 生态较差)

--- packages/components/src/markdown-renderer copy/utils.ts
+++ packages/components/src/markdown-renderer copy/utils.ts
@@ -1,13 +1,7 @@
 import MarkdownItPluginEcharts from '@lexmin0412/markdown-it-echarts'
-import classnames from 'classnames'
 import MarkdownIt from 'markdown-it'
 import markdownItPluginKatex from 'markdown-it-katex-gpt'
 import MarkdownItMermaid from 'markdown-it-mermaid'
-import { twMerge } from 'tailwind-merge'
-
-export const cn = (...cls: classnames.ArgumentArray) => {
-	return twMerge(classnames(cls))
-}
 
 const md = MarkdownIt({ html: true, breaks: true })
 	.use(MarkdownItPluginEcharts)
