00272262ff5d48841f639a09b6e857a97799dd65
lexmin0412 <zhangle_dev@outlook.com>
2025-04-16 18:27:40 +0800

    refactor: 部分聊天功能逻辑移入 components 子包, 简化应用层代码

--- src/components/chat-placeholder.tsx
+++ src/components/chat-placeholder.tsx
@@ -1,13 +1,8 @@
-import { MessageOutlined } from '@ant-design/icons'
 import { IGetAppInfoResponse, IGetAppParametersResponse } from '@dify-chat/api'
-import { useDifyChat } from '@dify-chat/core'
-import { Button, Empty, Form, FormItemProps, Input, Select } from 'antd'
-import { useHistory, useParams, useSearchParams } from 'pure-react-router'
-import { useEffect, useRef, useState } from 'react'
+import { Empty, FormInstance, FormItemProps } from 'antd'
 
-import { unParseGzipString } from '@/utils'
 
-import AppInfo from './app-info'
+import { AppInputForm } from '@dify-chat/components'
 
 export interface IConversationEntryFormItem extends FormItemProps {
 	type: 'input' | 'select'
@@ -33,122 +28,30 @@ interface IChatPlaceholderProps {
 	/**
 	 * 当前对话 ID
 	 */
-	conversationId?: string
+	conversationId: string
+	/**
+	 * 应用输入参数表单实例
+	 */
+	entryForm: FormInstance<any>
 }
 
 /**
  * 对话区域的占位组件（展示参数填写表单 / 应用信息）
  */
 export const ChatPlaceholder = (props: IChatPlaceholderProps) => {
-	const { formFilled, onStartConversation, user_input_form, appInfo, conversationId } = props
-	const history = useHistory()
-	const { appId } = useParams<{ appId: string }>()
-	const searchParams = useSearchParams()
-	const [userInputItems, setUserInputItems] = useState<IConversationEntryFormItem[]>([])
-	const cachedSearchParams = useRef<URLSearchParams>(new URLSearchParams(searchParams))
-	const { mode } = useDifyChat()
-
-	const [entryForm] = Form.useForm()
-
-	useEffect(() => {
-		entryForm.resetFields()
-	}, [conversationId])
-
-	useEffect(() => {
-		// 如果已经填写了，那就不需要了
-		if (formFilled || !user_input_form?.length) {
-			setUserInputItems([])
-		}
-		setUserInputItems(
-			user_input_form?.map(item => {
-				if (item['text-input']) {
-					const originalProps = item['text-input']
-					const baseProps: IConversationEntryFormItem = {
-						type: 'input',
-						label: originalProps.label,
-						name: originalProps.variable,
-					}
-					const searchValue = cachedSearchParams.current.get(originalProps.variable)
-					if (searchValue) {
-						console.log(
-							'unParseGzipString(searchValue)',
-							cachedSearchParams.current.get(originalProps.variable),
-							unParseGzipString(searchValue),
-						)
-						entryForm.setFieldValue(originalProps.variable, unParseGzipString(searchValue))
-						cachedSearchParams.current.delete(originalProps.variable)
-					}
-					if (originalProps.required) {
-						baseProps.required = true
-						baseProps.rules = [{ required: true, message: '请输入' }]
-					}
-					return baseProps
-				}
-				return {} as IConversationEntryFormItem
-			}) || [],
-		)
-		// 如果处理后的参数数量和之前的不一致，表示有表单相关，更新 URL
-		if (searchParams.size !== cachedSearchParams.current.size) {
-			if (cachedSearchParams.current.has('isNewCvst')) {
-				cachedSearchParams.current.delete('isNewCvst')
-			}
-			const searchString = cachedSearchParams.current.size
-				? `?${cachedSearchParams.current.toString()}`
-				: ''
-			if (mode === 'multiApp') {
-				history.push(`/app/${appId}${searchString}`)
-			} else {
-				history.push(`/chat${searchString}`)
-			}
-		}
-	}, [user_input_form])
+	const { formFilled, appInfo } = props
 
 	return (
 		<div className="w-full h-full flex items-center justify-center -mt-5">
 			<div className="max-w-[80vw] w-3/5 py-6 px-10 rounded-3xl bg-gray-100 box-border">
 				{appInfo ? (
-					<>
-						<AppInfo info={appInfo} />
-						{!formFilled && user_input_form?.length ? (
-							<Form
-								form={entryForm}
-								className="mt-6"
-								labelCol={{ span: 5 }}
-							>
-								{userInputItems.map(item => {
-									return (
-										<Form.Item
-											key={item.name}
-											name={item.name}
-											label={item.label}
-											required={item.required}
-											rules={item.rules}
-										>
-											{item.type === 'input' ? (
-												<Input placeholder="请输入" />
-											) : item.type === 'select' ? (
-												<Select placeholder="请选择" />
-											) : (
-												'不支持的控件类型'
-											)}
-										</Form.Item>
-									)
-								})}
-							</Form>
-						) : null}
-						<div className="mt-3 w-full flex justify-center">
-							<Button
-								type="primary"
-								icon={<MessageOutlined />}
-								onClick={async () => {
-									await entryForm.validateFields()
-									onStartConversation(entryForm.getFieldsValue())
-								}}
-							>
-								开始对话
-							</Button>
-						</div>
-					</>
+					<AppInputForm 
+						formFilled={formFilled}
+						onStartConversation={props.onStartConversation}
+						user_input_form={props.user_input_form}
+						conversationId={props.conversationId}
+						entryForm={props.entryForm}
+					/>
 				) : (
 					<Empty description="请先配置 Dify 应用" />
 				)}
--- src/layout/base-layout.tsx
+++ src/layout/base-layout.tsx
@@ -3,11 +3,12 @@ import { XProvider } from '@ant-design/x'
 import {
 	createDifyApiInstance,
 	DifyApi,
+	IConversationItem,
 	IGetAppInfoResponse,
 	IGetAppParametersResponse,
 } from '@dify-chat/api'
-import { ConversationList, type IConversationItem } from '@dify-chat/components'
-import { IDifyAppItem, IDifyChatContextMultiApp } from '@dify-chat/core'
+import { ConversationList } from '@dify-chat/components'
+import { ConversationsContextProvider, IDifyAppItem, IDifyChatContextMultiApp } from '@dify-chat/core'
 import { useDifyChat } from '@dify-chat/core'
 import { Button, Divider, Empty, message, Space, Spin } from 'antd'
 import { createStyles } from 'antd-style'
@@ -17,11 +18,12 @@ import React, { useEffect, useMemo, useState } from 'react'
 import ChatboxWrapper from '@/components/chatbox-wrapper'
 import { GithubIcon, Logo } from '@/components/logo'
 import { DEFAULT_CONVERSATION_NAME } from '@/constants'
-import { useMap4Arr } from '@/hooks/use-map-4-arr'
 import { colors } from '@/theme/config'
 
 import './../App.css'
 import CenterTitleWrapper from './components/center-title-wrapper'
+import { isTempId } from '@dify-chat/helpers'
+import dayjs from 'dayjs'
 
 const useStyle = createStyles(({ token, css }) => {
 	return {
@@ -75,6 +77,13 @@ const BaseLayout = (props: IBaseLayoutProps) => {
 		initLoading,
 	} = props
 	const { ...difyChatContext } = useDifyChat()
+	
+	const [conversations, setConversations] = useState<IConversationItem[]>([])
+	const [currentConversationId, setCurrentConversationId] = useState<string>('')
+	const currentConversationInfo = useMemo(() => {
+		return conversations.find(item => item.id === currentConversationId)
+	}, [conversations, currentConversationId])
+
 	const { user } = difyChatContext as IDifyChatContextMultiApp
 	// 创建 Dify API 实例
 	const { styles } = useStyle()
@@ -86,30 +95,10 @@ const BaseLayout = (props: IBaseLayoutProps) => {
 		}),
 	)
 	const searchParams = useSearchParams()
-	const [conversationsItems, setConversationsItems] = useState<IConversationItem[]>([])
-	// 优化会话列表查找逻辑（高频操作）
-	const conversationMap = useMap4Arr<IConversationItem>(conversationsItems, 'key')
 	const [conversationListLoading, setCoversationListLoading] = useState<boolean>(false)
-	const [currentConversationId, setCurrentConversationId] = useState<string>()
 	const [appInfo, setAppInfo] = useState<IGetAppInfoResponse>()
 	const [appParameters, setAppParameters] = useState<IGetAppParametersResponse>()
 	const [appConfigLoading, setAppConfigLoading] = useState(false)
-	const [inputParams, setInputParams] = useState<{ [key: string]: unknown }>({})
-
-	/**
-	 * 重置输入表单值
-	 */
-	const resetFormValues = () => {
-		// 遍历 inputParams 置为 undefined
-		if (appParameters?.user_input_form?.length) {
-			const newInputParams = { ...inputParams }
-			appParameters?.user_input_form.forEach(item => {
-				const field = item['text-input']
-				newInputParams[field.variable] = undefined
-			})
-			setInputParams(newInputParams)
-		}
-	}
 
 	const initAppInfo = async () => {
 		setAppInfo(undefined)
@@ -136,8 +125,7 @@ const BaseLayout = (props: IBaseLayoutProps) => {
 				}
 			})
 		})
-		resetFormValues()
-		setCurrentConversationId(undefined)
+		setCurrentConversationId('')
 	})
 
 	/**
@@ -154,8 +142,12 @@ const BaseLayout = (props: IBaseLayoutProps) => {
 						label: item.name,
 					}
 				}) || []
-			setConversationsItems(newItems)
-			setCurrentConversationId(newItems[0]?.key)
+			setConversations(result?.data)
+			if (newItems.length) {
+				setCurrentConversationId(newItems[0]?.key)
+			} else {
+				onAddConversation()
+			}
 		} catch (error) {
 			console.error(error)
 			message.error(`获取会话列表失败: ${error}`)
@@ -171,11 +163,16 @@ const BaseLayout = (props: IBaseLayoutProps) => {
 		// 创建新对话
 		const newKey = `temp_${Math.random()}`
 		// 使用函数式更新保证状态一致性（修复潜在竞态条件）
-		setConversationsItems(prev => {
+		setConversations(prev => {
 			return [
 				{
-					key: newKey,
-					label: DEFAULT_CONVERSATION_NAME,
+					id: newKey,
+					name: DEFAULT_CONVERSATION_NAME,
+					created_at: dayjs().valueOf(),
+					inputs: {},
+					introduction: '',
+					status: 'normal',
+					updated_at: dayjs().valueOf()
 				},
 				...prev,
 			]
@@ -184,152 +181,145 @@ const BaseLayout = (props: IBaseLayoutProps) => {
 	}
 
 	useEffect(() => {
-		// 如果对话 ID 不在当前列表中，则刷新一下
-		if (currentConversationId && !conversationMap.has(currentConversationId)) {
-			getConversationItems()
-		}
-	}, [currentConversationId])
-
-	const conversationName = useMemo(() => {
-		return (
-			conversationsItems.find(item => item.key === currentConversationId)?.label ||
-			DEFAULT_CONVERSATION_NAME
-		)
-	}, [conversationsItems, currentConversationId])
-
-	useEffect(() => {
 		if (!appConfig) {
-			setConversationsItems([])
+			setConversations([])
 			setAppInfo(undefined)
 			setCurrentConversationId('')
-			resetFormValues()
 		}
 	}, [appConfig])
 
 	return (
 		<XProvider theme={{ token: { colorPrimary: colors.primary, colorText: colors.default } }}>
-			<div
-				className={`w-full h-screen ${styles.layout} flex flex-col overflow-hidden bg-[#eff0f5]`}
-			>
-				{/* 头部 */}
-				<div className="hidden md:!flex items-center justify-between px-6">
-					{/* 🌟 Logo */}
-					<div className={`flex-1 overflow-hidden ${appConfig ? '' : 'shadow-sm'}`}>
-						<Logo hideGithubIcon />
-					</div>
+			<ConversationsContextProvider value={{ conversations, setConversations, currentConversationId, setCurrentConversationId, currentConversationInfo }}>
+				<div
+					className={`w-full h-screen ${styles.layout} flex flex-col overflow-hidden bg-[#eff0f5]`}
+				>
+					{/* 头部 */}
+					<div className="hidden md:!flex items-center justify-between px-6">
+						{/* 🌟 Logo */}
+						<div className={`flex-1 overflow-hidden ${appConfig ? '' : 'shadow-sm'}`}>
+							<Logo hideGithubIcon />
+						</div>
 
-					<CenterTitleWrapper>
-						{renderCenterTitle ? renderCenterTitle(appInfo!) : null}
-					</CenterTitleWrapper>
+						<CenterTitleWrapper>
+							{renderCenterTitle ? renderCenterTitle(appInfo!) : null}
+						</CenterTitleWrapper>
 
-					{/* 右侧图标 */}
-					<div className="flex-1 overflow-hidden">
-						<div className="flex items-center justify-end text-sm">
-							<Space split={<Divider type="vertical" />}>
-								<GithubIcon />
-							</Space>
+						{/* 右侧图标 */}
+						<div className="flex-1 overflow-hidden">
+							<div className="flex items-center justify-end text-sm">
+								<Space split={<Divider type="vertical" />}>
+									<GithubIcon />
+								</Space>
+							</div>
 						</div>
 					</div>
-				</div>
 
-				{/* Main */}
-				<div className="flex-1 overflow-hidden flex rounded-3xl bg-white">
-					{appConfigLoading || initLoading ? (
-						<div className="absolute w-full h-full left-0 top-0 z-50 flex items-center justify-center">
-							<Spin spinning />
-						</div>
-					) : appConfig ? (
-						<>
-							{/* 左侧对话列表 */}
-							<div className={`${styles.menu} hidden md:!flex w-72 h-full flex-col`}>
-								{/* 添加会话 */}
-								{appConfig ? (
-									<Button
-										onClick={() => {
-											resetFormValues()
-											onAddConversation()
-										}}
-										className="h-10 leading-10 border border-solid border-gray-200 w-[calc(100%-24px)] mt-3 mx-3 text-default"
-										icon={<PlusOutlined />}
-									>
-										新增对话
-									</Button>
-								) : null}
-								{/* 🌟 对话管理 */}
-								<div className="px-3">
-									<Spin spinning={conversationListLoading}>
-										{conversationsItems?.length ? (
-											<ConversationList
-												renameConversationPromise={(conversationId: string, name: string) =>
-													difyApi?.renameConversation({
-														conversation_id: conversationId,
-														name,
-													})
-												}
-												deleteConversationPromise={difyApi?.deleteConversation}
-												items={conversationsItems}
-												activeKey={currentConversationId}
-												onActiveChange={id => {
-													resetFormValues()
-													setCurrentConversationId(id)
-												}}
-												onItemsChange={setConversationsItems}
-												refreshItems={getConversationItems}
-											/>
-										) : (
-											<div className="w-full h-full flex items-center justify-center">
-												<Empty
-													className="pt-6"
-													description="暂无会话"
+					{/* Main */}
+					<div className="flex-1 overflow-hidden flex rounded-3xl bg-white">
+						{appConfigLoading || initLoading ? (
+							<div className="absolute w-full h-full left-0 top-0 z-50 flex items-center justify-center">
+								<Spin spinning />
+							</div>
+						) : appConfig ? (
+							<>
+								{/* 左侧对话列表 */}
+								<div className={`${styles.menu} hidden md:!flex w-72 h-full flex-col`}>
+									{/* 添加会话 */}
+									{appConfig ? (
+										<Button
+											onClick={() => {
+												onAddConversation()
+											}}
+											className="h-10 leading-10 border border-solid border-gray-200 w-[calc(100%-24px)] mt-3 mx-3 text-default"
+											icon={<PlusOutlined />}
+										>
+											新增对话
+										</Button>
+									) : null}
+									{/* 🌟 对话管理 */}
+									<div className="px-3">
+										<Spin spinning={conversationListLoading}>
+											{conversations?.length ? (
+												<ConversationList
+													renameConversationPromise={async (conversationId: string, name: string) => {
+														await difyApi?.renameConversation({
+															conversation_id: conversationId,
+															name,
+														})
+														getConversationItems()
+													}}
+													deleteConversationPromise={async (conversationId: string) => {
+														if (isTempId(conversationId)) {
+															const newConversations = conversations.filter((item) => item.id !== conversationId)
+															setConversations(newConversations)
+															if (conversationId === currentConversationId && newConversations.length) {
+																setCurrentConversationId(newConversations[0].id)
+															}
+														} else {
+															await difyApi?.deleteConversation(conversationId)
+															getConversationItems()
+															return Promise.resolve()
+														}
+													}}
+													items={conversations.map((item)=>{
+														return {
+															key: item.id,
+															label: item.name
+														}
+													})}
+													activeKey={currentConversationId}
+													onActiveChange={id => {
+														setCurrentConversationId(id)
+													}}
 												/>
-											</div>
-										)}
-									</Spin>
+											) : (
+												<div className="w-full h-full flex items-center justify-center">
+													<Empty
+														className="pt-6"
+														description="暂无会话"
+													/>
+												</div>
+											)}
+										</Spin>
+									</div>
 								</div>
-							</div>
 
-							{/* 右侧聊天窗口 - 移动端全屏 */}
-							<div className="flex-1 min-w-0 flex flex-col overflow-hidden">
-								<ChatboxWrapper
-									inputParams={inputParams}
-									setInputParams={setInputParams}
-									resetFormValues={resetFormValues}
-									appConfig={appConfig}
-									appConfigLoading={appConfigLoading}
-									appInfo={appInfo}
-									difyApi={difyApi}
-									conversationId={currentConversationId}
-									conversationName={conversationName}
-									conversationItems={conversationsItems}
-									onConversationIdChange={setCurrentConversationId}
-									appParameters={appParameters}
-									conversationListLoading={conversationListLoading}
-									onAddConversation={onAddConversation}
-									onItemsChange={setConversationsItems}
-									conversationItemsChangeCallback={getConversationItems}
-								/>
-							</div>
-						</>
-					) : (
-						<div className="w-full h-full flex items-center justify-center">
-							<Empty
-								description="暂无 Dify 应用配置"
-								className="text-base"
-							>
-								<Button
-									size="large"
-									type="primary"
-									onClick={handleStartConfig}
+								{/* 右侧聊天窗口 - 移动端全屏 */}
+								<div className="flex-1 min-w-0 flex flex-col overflow-hidden">
+									<ChatboxWrapper
+										appConfig={appConfig}
+										appConfigLoading={appConfigLoading}
+										appInfo={appInfo}
+										difyApi={difyApi}
+										appParameters={appParameters}
+										conversationListLoading={conversationListLoading}
+										onAddConversation={onAddConversation}
+										conversationItemsChangeCallback={getConversationItems}
+									/>
+								</div>
+							</>
+						) : (
+							<div className="w-full h-full flex items-center justify-center">
+								<Empty
+									description="暂无 Dify 应用配置"
+									className="text-base"
 								>
-									开始配置
-								</Button>
-							</Empty>
-						</div>
-					)}
+									<Button
+										size="large"
+										type="primary"
+										onClick={handleStartConfig}
+									>
+										开始配置
+									</Button>
+								</Empty>
+							</div>
+						)}
+					</div>
 				</div>
-			</div>
 
-			{extComponents}
+				{extComponents}
+			</ConversationsContextProvider>
 		</XProvider>
 	)
 }
