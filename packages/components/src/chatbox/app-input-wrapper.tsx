6fd7912d89bb9b2a261a85deee3594a9ae2581c6
lexmin0412 <zhangle_dev@outlook.com>
2025-04-17 23:06:31 +0800

    optimize: 调整对话参数设置样式和展示位置, 优化布局

--- packages/components/src/chatbox/app-input-wrapper.tsx
+++ packages/components/src/chatbox/app-input-wrapper.tsx
@@ -1,51 +1,63 @@
-import {
-  CaretRightOutlined,
-} from '@ant-design/icons'
+import { CaretRightOutlined } from '@ant-design/icons'
+import { isTempId } from '@dify-chat/helpers'
 import { Collapse, CollapseProps, theme } from 'antd'
-import { CSSProperties } from 'react'
+import { CSSProperties, useEffect, useState } from 'react'
+
 import AppInputForm, { IAppInputFormProps } from './app-input-form'
 
 /**
  * 应用输入参数管理容器
  */
 export default function AppInputWrapper(props: IAppInputFormProps) {
-  const { token } = theme.useToken();
+	const { token } = theme.useToken()
+	const [activeKey, setActiveKey] = useState<string[]>([])
+	const { conversationId } = props
 
-  if (!props.user_input_form?.length) {
-    return null
-  }
+	useEffect(() => {
+		if (isTempId(conversationId)) {
+			setActiveKey(['1'])
+		} else {
+			setActiveKey([])
+		}
+	}, [conversationId])
 
-  const getItems: (panelStyle: CSSProperties) => CollapseProps['items'] = (panelStyle) => [
-    {
-      key: '1',
-      label: <div>对话参数设置</div>,
-      children: <AppInputForm
-        formFilled={props.formFilled}
-        onStartConversation={props.onStartConversation}
-        user_input_form={props.user_input_form}
-        conversationId={props.conversationId}
-        entryForm={props.entryForm}
-      />,
-      style: panelStyle,
-    },
-  ];
+	if (!props.user_input_form?.length) {
+		return null
+	}
 
-  const panelStyle: React.CSSProperties = {
-    marginBottom: 24,
-    background: token.colorWhite,
-    color: token.colorText,
-    borderRadius: token.borderRadiusLG,
-    border: `2px solid #1669ee99`,
-  };
+	const getItems: (panelStyle: CSSProperties) => CollapseProps['items'] = panelStyle => [
+		{
+			key: '1',
+			label: <div className="font-semibold text-base">对话参数设置</div>,
+			children: (
+				<AppInputForm
+					formFilled={props.formFilled}
+					onStartConversation={props.onStartConversation}
+					user_input_form={props.user_input_form}
+					conversationId={props.conversationId}
+					entryForm={props.entryForm}
+				/>
+			),
+			style: panelStyle,
+		},
+	]
 
-  return (
-    <Collapse
-      className='mt-3 bg-primary'
-      bordered={false}
-      defaultActiveKey={['1']}
-      expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />}
-      style={{ background: token.colorBgContainer, borderColor: token.colorPrimary }}
-      items={getItems(panelStyle)}
-    />
-  )
+	const panelStyle: React.CSSProperties = {
+		color: token.colorText,
+		borderRadius: '8px',
+		border: `1px solid #eff0f5`,
+	}
+
+	return (
+		<Collapse
+			className="mt-3 bg-primary"
+			bordered={false}
+			activeKey={activeKey}
+			onChange={value => setActiveKey(value)}
+			expandIconPosition="end"
+			expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />}
+			style={{ background: token.colorBgContainer, borderColor: token.colorPrimary }}
+			items={getItems(panelStyle)}
+		/>
+	)
 }
