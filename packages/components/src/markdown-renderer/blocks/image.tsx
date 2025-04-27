b68d0d626a97e78c4ba76087ce075e559bab9486
lexmin0412 <zhangle_dev@outlook.com>
2025-04-27 12:52:44 +0800

    feat(Chatbox): AI 回复消息中的图片支持点击放大查看

--- packages/components/src/markdown-renderer/blocks/image.tsx
+++ packages/components/src/markdown-renderer/blocks/image.tsx
@@ -1,4 +1,5 @@
-import classNames from "classnames"
+import classNames from 'classnames'
+import { PhotoProvider, PhotoView } from 'react-photo-view'
 
 interface IImageProps {
 	className?: string
@@ -10,13 +11,22 @@ interface IImageProps {
  * 图片渲染组件
  */
 export default function ImageBlock(props: IImageProps) {
-
 	const { className, src, alt } = props
 
 	const imgClassNames = classNames({
-		"w-full h-full max-w-[500px] rounded-lg mt-2": true,
-		[`${className}`]: !!className
+		'w-full h-full max-w-[500px] rounded-lg mt-2 cursor-zoom-in': true,
+		[`${className}`]: !!className,
 	})
 
-	return <img className={imgClassNames} alt={alt || '图片加载失败'} src={src} />
+	return (
+		<PhotoProvider>
+			<PhotoView src={src}>
+				<img
+					className={imgClassNames}
+					alt={alt || '图片加载失败'}
+					src={src}
+				/>
+			</PhotoView>
+		</PhotoProvider>
+	)
 }
