3d04211c0cb0da37181bdbde39761ec9e61b09b6
lexmin0412 <zhangle_dev@outlook.com>
2025-04-29 14:00:24 +0800

    fix(mobile): 修复应用标题超长时, 标题容器圆角消失的问题

--- src/layout/components/center-title-wrapper.tsx
+++ src/layout/components/center-title-wrapper.tsx
@@ -4,7 +4,7 @@
 export default function CenterTitleWrapper(props: { children: React.ReactNode }) {
 	return (
 		<div className="flex h-full items-center flex-[4] overflow-hidden justify-center text-primary font-semibold">
-			<div className="flex items-center rounded-3xl shadow-md py-2 px-4 text-sm bg-white">
+			<div className="flex items-center rounded-3xl shadow-md py-2 px-4 text-sm bg-white overflow-hidden">
 				{props.children}
 			</div>
 		</div>
