b87d78d585b41473514ab461fe5218126e9ac002
lexmin0412 <zhangle_dev@outlook.com>
2025-05-14 14:20:01 +0800

    feat: 新增应用列表的静态配置实现

---
+++ src/services/app/static-readonly/index.ts
@@ -0,0 +1,21 @@
+import { DifyAppStoreReadonly, IDifyAppItem } from '@dify-chat/core'
+
+import { staticAppList } from './data'
+
+/**
+ * 应用列表的静态配置实现
+ * 注意：**尽量不要在公开的生产环境中使用静态数据**，推荐使用后端服务
+ */
+class DifyAppService extends DifyAppStoreReadonly {
+	public readonly = true as const
+
+	async getApps(): Promise<IDifyAppItem[]> {
+		return Promise.resolve(staticAppList)
+	}
+
+	async getApp(id: string): Promise<IDifyAppItem | undefined> {
+		return Promise.resolve(staticAppList.find(config => config.id === id))
+	}
+}
+
+export default DifyAppService
