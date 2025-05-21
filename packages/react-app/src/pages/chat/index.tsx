5aecefc33d39dafed850f4d71347cc5df24c63fc
lexmin0412 <zhangle_dev@outlook.com>
2025-05-21 18:25:28 +0800

    docs(react-app): 更新用户标识注释中对于 Dify API 的一些表述

--- packages/react-app/src/pages/chat/index.tsx
+++ packages/react-app/src/pages/chat/index.tsx
@@ -8,7 +8,7 @@ import SingleAppLayout from '@/layout/single-app-layout'
 export default function ChatPage() {
 	const { user, mode } = useDifyChat()
 
-	// 必须先有用户, 再开始渲染布局，因为所有界面上展示的数据都需要调用 Dify API, 而所有的 Dify API 都需要用户标识
+	// 必须先有用户, 再开始渲染布局，因为所有界面上展示的数据都需要调用 Dify API, 而大部分的 Dify API 都需要用户标识
 	if (!user) {
 		return (
 			<div className="w-screen h-screen flex flex-col items-center justify-center bg-theme-bg">
