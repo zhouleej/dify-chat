56aead0dcede55bff83939cd4065b9024e47cfed
lexmin0412 <zhangle_dev@outlook.com>
2025-05-27 17:22:41 +0800

    feat(nextjs-app): 接入 antd

--- packages/nextjs-app/src/app/apps/page.tsx
+++ packages/nextjs-app/src/app/apps/page.tsx
@@ -1,3 +1,11 @@
+import { Button } from "antd";
+
 export default function AppsPage() {
-	return <div>AppsPage2</div>;
+	return (
+		<div>
+			<div className="flex items-center">应用列表</div>
+			应用列表
+			<Button type="primary">创建应用</Button>
+		</div>
+	);
 }
--- packages/nextjs-app/src/app/layout.tsx
+++ packages/nextjs-app/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { AntdRegistry } from "@ant-design/nextjs-registry";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +28,7 @@ export default function RootLayout({
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
-				{children}
+				<AntdRegistry>{children}</AntdRegistry>
 			</body>
 		</html>
 	);
