e96ccd36d404c7bf67fdb69e38c4c18dff61cde2
lexmin0412 <zhangle_dev@outlook.com>
2025-05-27 18:16:20 +0800

    feat(nextjs-app): antd 兼容 react19

--- packages/nextjs-app/src/app/layout.tsx
+++ packages/nextjs-app/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 import "./globals.css";
+import "@ant-design/v5-patch-for-react-19";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
