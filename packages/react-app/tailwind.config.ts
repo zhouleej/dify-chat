c1aa73efa7cf06cac70224efad4518be3e24c8c8
lexmin0412 <zhangle_dev@outlook.com>
2025-05-27 17:24:07 +0800

    chore(react-app): 优化注释

--- packages/react-app/tailwind.config.ts
+++ packages/react-app/tailwind.config.ts
@@ -11,7 +11,7 @@ const minWidth = {
 
 const content = ['./src/**/*.{html,js,ts,jsx,tsx}']
 
-// 则添加 packages/components 目录
+// 添加 packages/components 目录
 content.push('../components/src/**/*.{html,js,ts,jsx,tsx}')
 
 const config: Config = {
