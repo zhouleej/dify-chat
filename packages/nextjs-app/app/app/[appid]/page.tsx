f070f56ca45d570951084e1af2f8a0686e735bcd
lexmin0412 <zhangle_dev@outlook.com>
2025-06-02 17:56:37 +0800

    optimize: 添加 ant-design 对 react 19 的补丁
    补丁需要放在页面入口才能生效，因此将其添加到app/[appid]/page.tsx文件顶部

--- packages/nextjs-app/app/app/[appid]/page.tsx
+++ packages/nextjs-app/app/app/[appid]/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+// 只有放在页面入口才能生效
+import "@ant-design/v5-patch-for-react-19";
+
 import { DifyChatProvider } from "@dify-chat/core";
 import DifyAppService from "@/services/app/apps";
 import MultiAppLayout from "./layout/multi-app-layout";
