b059ab4233db52d143abd87279740c660405998b
lexmin0412 <zhangle_dev@outlook.com>
2025-06-02 17:55:38 +0800

    refactor(nextjs-app): 移除谷歌字体使用，规避网络限制导致的字体加载失败

--- packages/nextjs-app/app/layout.tsx
+++ packages/nextjs-app/app/layout.tsx
@@ -1,18 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import "@ant-design/v5-patch-for-react-19";
 import EntryWrapper from "./entry-wrapper";
 
-const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
-});
+// const geistSans = Geist({
+// 	variable: "--font-geist-sans",
+// 	subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+// 	variable: "--font-geist-mono",
+// 	subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
 	title: "Dify Chat Next",
@@ -27,7 +26,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+			// className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
 				<EntryWrapper>{children}</EntryWrapper>
 			</body>
