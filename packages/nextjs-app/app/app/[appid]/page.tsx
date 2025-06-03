a41c62eb7837a805839a148c54afd1436b4fb9b3
lexmin0412 <zhangle_dev@outlook.com>
2025-06-03 22:52:35 +0800

    feat(nextjs-app): 添加未登录用户的加载状态界面，提升用户体验的同时解决 difyApi 在 useX 中的闭包问题

--- packages/nextjs-app/app/app/[appid]/page.tsx
+++ packages/nextjs-app/app/app/[appid]/page.tsx
@@ -7,9 +7,24 @@ import { DifyChatProvider } from "@dify-chat/core";
 import DifyAppService from "@/services/app/apps";
 import MultiAppLayout from "./layout/multi-app-layout";
 import { useUserId } from "@/hooks/useUserId";
+import { Logo } from "@/components";
+import { Spin } from "antd";
 
 export default function AppPage() {
 	const user = useUserId();
+	if (!user) {
+		return (
+			<div className="w-screen h-screen flex flex-col items-center justify-center bg-theme-bg">
+				<div className="absolute flex-col w-full h-full left-0 top-0 z-50 flex items-center justify-center">
+					<Logo hideGithubIcon />
+					<div className="text-theme-text">授权登录中...</div>
+					<div className="mt-6">
+						<Spin spinning />
+					</div>
+				</div>
+			</div>
+		);
+	}
 	return (
 		<DifyChatProvider
 			value={{
