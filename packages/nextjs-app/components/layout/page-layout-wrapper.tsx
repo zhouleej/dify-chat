536d00347cb7939091edc7b04cd1ee72571bd63c
lexmin0412 <zhangle_dev@outlook.com>
2025-06-07 16:39:03 +0800

    feat(nextjs-app/layout): 添加页面布局包装组件以支持主题切换
    新增 PageLayoutWrapper 组件，用于为 Ant Design 的 ConfigProvider 提供主题切换功能
    通过 ThemeContextWrapper 实现暗黑/默认主题的动态切换

---
+++ packages/nextjs-app/components/layout/page-layout-wrapper.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { ThemeContextProvider, useThemeContext } from "@dify-chat/theme";
+import { ConfigProvider, theme } from "antd";
+import React from "react";
+
+const ThemeContextWrapper = ({ children }: { children: React.ReactNode }) => {
+	const { isDark } = useThemeContext();
+	return (
+		<ConfigProvider
+			theme={{
+				algorithm: isDark ? theme.darkAlgorithm : theme.defaultAlgorithm,
+			}}
+		>
+			{children}
+		</ConfigProvider>
+	);
+};
+
+/**
+ * 主要的作用是为 AntD 的 ConfigProvider 提供主题获取功能
+ */
+export default function PageLayoutWrapper({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<ThemeContextProvider>
+			<ThemeContextWrapper>{children}</ThemeContextWrapper>
+		</ThemeContextProvider>
+	);
+}
