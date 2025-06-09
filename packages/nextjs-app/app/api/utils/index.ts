97cf5642032231fef7fabe8d708408e05ec6692e
lexmin0412 <zhangle_dev@outlook.com>
2025-06-09 13:47:22 +0800

    refactor(nextjs-app/api): 使用核心库方法重构生成 Dify 请求逻辑
    用 @dify-chat/core 提供的 genDifyRequestByRequestConfig 方法替代手动构建 BaseRequest 实例，简化代码并提高可维护性

--- packages/nextjs-app/app/api/utils/index.ts
+++ packages/nextjs-app/app/api/utils/index.ts
@@ -1,17 +1,16 @@
 import { getAppItem } from "@/app/api-utils";
-import { BaseRequest } from "@dify-chat/helpers";
+import {
+	genDifyRequestByRequestConfig,
+	IDifyAppRequestConfig,
+} from "@dify-chat/core";
 
 /**
  * 生成 Dify 请求函数
  */
 export const genDifyRequest = async (appId: string) => {
 	const appItem = await getAppItem(appId);
-	// 从配置列表中找到 requestConfig 再发起请求获取
-	const request = new BaseRequest({
-		baseURL: appItem?.requestConfig?.apiBase || "",
-		headers: {
-			Authorization: `Bearer ${appItem?.requestConfig.apiKey as string}`,
-		},
-	});
+	const request = genDifyRequestByRequestConfig(
+		appItem?.requestConfig as IDifyAppRequestConfig,
+	);
 	return request;
 };
