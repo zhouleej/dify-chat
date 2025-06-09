a8111e604a7d34d475744fd8547db658ea1d185d
lexmin0412 <zhangle_dev@outlook.com>
2025-06-09 13:16:29 +0800

    feat(nextjs-app/middleware): 添加单应用模式下的路由重定向逻辑
    在单应用模式下访问多应用页面时，将请求重定向到首页由首页处理分流

---
+++ packages/nextjs-app/middleware.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { getConfigs } from "./config/env";
+
+export function middleware(request: NextRequest) {
+	const { pathname } = request.nextUrl;
+	const { runningMode } = getConfigs();
+	// 如果是单应用模式下访问了多应用的页面，则重定向到首页，由首页自行分流到单应用页面
+	if (pathname.startsWith("/app")) {
+		if (runningMode === "singleApp") {
+			return NextResponse.rewrite(new URL("/", request.url));
+		}
+	}
+}
