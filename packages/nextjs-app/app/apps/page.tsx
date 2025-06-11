c6b81c87ef23dcf4ac01e89e5ab7cb2399b50d19
lexmin0412 <zhangle_dev@outlook.com>
2025-06-11 13:56:43 +0800

    feat(nextjs-app): 新增 createApp 和 deleteApp 的 server actions

--- packages/nextjs-app/app/apps/page.tsx
+++ packages/nextjs-app/app/apps/page.tsx
@@ -1,10 +1,36 @@
-import AppsPageMain from "./content-client";
-import { getUser } from "../actions";
-import { getAppList } from "./actions";
+import { getUser } from "@/app/actions";
+import { getAppList } from "@/app/apps/actions";
 
-export default function AppsPage() {
-	// 定义用于 Client 组件的 Promise 请求
-	const user = getUser();
-	const getApps = getAppList();
-	return <AppsPageMain user={user} getApps={getApps} />;
+import { Empty, Row } from "antd";
+
+import AddButton from "@/app/apps/components/add-button";
+import AppItem from "@/app/apps/components/app-item";
+import Header from "@/app/apps/components/header";
+
+export default async function AppsPage() {
+	const user = await getUser();
+	const apps = await getAppList();
+
+	const refreshApps = getAppList;
+
+	return (
+		<div className="h-screen relative overflow-hidden flex flex-col bg-theme-bg w-full">
+			<Header />
+			<div className="flex-1 bg-theme-main-bg rounded-3xl py-6 overflow-y-auto box-border overflow-x-hidden">
+				{apps?.length ? (
+					<Row gutter={[16, 16]} className="px-3 md:px-6">
+						{apps.map((item) => {
+							return <AppItem key={item.id} item={item} user={user} />;
+						})}
+					</Row>
+				) : (
+					<div className="w-full h-full box-border flex flex-col items-center justify-center px-3">
+						<Empty description="暂无应用" />
+					</div>
+				)}
+			</div>
+
+			{user.enableSetting ? <AddButton refreshApps={refreshApps} /> : null}
+		</div>
+	);
 }
