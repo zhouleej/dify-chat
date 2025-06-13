4e08c4fcfdd4d75d31ce8f7ede41619bff7377fd
lexmin0412 <zhangle_dev@outlook.com>
2025-06-13 15:27:25 +0800

    docs: 更新注释

--- packages/nextjs-app/app/auth/login/actions.ts
+++ packages/nextjs-app/app/auth/login/actions.ts
@@ -5,7 +5,7 @@ import { createSession } from "@/lib/session";
 
 /**
  * 登录 server action
- * 简单的用户登录实现，可以改造为用户信息表单，如手机号+验证码，手机号+密码等等
+ * 简单的用户登录实现，可以改造为用户信息表单，如账号+密码
  */
 export const loginAction = async (userId: string) => {
 	return createSession(userId);
