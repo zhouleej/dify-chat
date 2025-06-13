a7bec61ae286151f11cdbb4fb243d2aa5cb42d1e
lexmin0412 <zhangle_dev@outlook.com>
2025-06-13 15:02:49 +0800

    fix(nextjs-app): 修复多余的分号导致的布局移动

--- packages/nextjs-app/app/layout.tsx
+++ packages/nextjs-app/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
 			<body
 			// className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
-				<AntdRegistry>{children}</AntdRegistry>;
+				<AntdRegistry>{children}</AntdRegistry>
 			</body>
 		</html>
 	);
