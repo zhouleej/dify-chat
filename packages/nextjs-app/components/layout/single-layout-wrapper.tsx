2245f8fcbbfb49006c123b7bc82f7c23aae5312a
lexmin0412 <zhangle_dev@outlook.com>
2025-06-17 21:51:21 +0800

    refactor(nextjs-app/layout): 单应用模式使用 PageLayoutWrapper 容器包裹，兼容 React19 及主题上下文

--- packages/nextjs-app/components/layout/single-layout-wrapper.tsx
+++ packages/nextjs-app/components/layout/single-layout-wrapper.tsx
@@ -1,17 +1,20 @@
 "use client";
 import SingleAppLayout from "@/app/app/[appId]/layout/single-app-layout";
 import { DifyChatProvider, IDifyAppItem } from "@dify-chat/core";
+import PageLayoutWrapper from "./page-layout-wrapper";
 
 export default function SingleAppLayoutWrapper(props: { app: IDifyAppItem }) {
 	return (
-		<DifyChatProvider
-			value={{
-				appConfig: props.app,
-				mode: "singleApp",
-				user: "lexmin",
-			}}
-		>
-			<SingleAppLayout />
-		</DifyChatProvider>
+		<PageLayoutWrapper>
+			<DifyChatProvider
+				value={{
+					appConfig: props.app,
+					mode: "singleApp",
+					user: "lexmin",
+				}}
+			>
+				<SingleAppLayout />
+			</DifyChatProvider>
+		</PageLayoutWrapper>
 	);
 }
