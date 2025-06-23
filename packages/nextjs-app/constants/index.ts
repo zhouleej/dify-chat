3f86e76c05fd9600660537bb47eadbb7159084b7
lexmin0412 <zhangle_dev@outlook.com>
2025-06-23 17:24:04 +0800

    feat(nextjs-app/constnats): 添加系统配置键常量文件

---
+++ packages/nextjs-app/constants/index.ts
@@ -0,0 +1,13 @@
+/**
+ * 系统配置的 Keys
+ */
+export const SystemConfigKeys = {
+	/**
+	 * 运行模式
+	 */
+	RunningMode: "running_mode",
+	/**
+	 * 单应用模式下正在运行的应用 ID
+	 */
+	RunningSingleAppId: "running_single_app_id",
+};
