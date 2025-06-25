ed4a8bc9228b50536d31564d1462d2c37699bfac
lexmin0412 <zhangle_dev@outlook.com>
2025-06-25 22:41:25 +0800

    chore(nextjs-app): 移除所有 prisma/drizzle ORM 相关的依赖和代码

--- packages/nextjs-app/lib/drizzle/index.ts
+++
@@ -1,7 +0,0 @@
-import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
-const pool = new Pool({
-	connectionString: process.env.DATABASE_URL!,
-	idleTimeoutMillis: 3600, // 空闲连接超时时间
-});
-export const db = drizzle({ client: pool });
--- packages/nextjs-app/prisma/migrations/20250623063830_add_system_config/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "system_config" (
-    "id" VARCHAR(36) NOT NULL,
-    "key" VARCHAR(255) NOT NULL,
-    "value" VARCHAR(255) NOT NULL,
-
-    CONSTRAINT "system_config_pkey" PRIMARY KEY ("id")
-);
