2a418bef9c2616f16202996f394f4be3c4a5ab13
lexmin0412 <zhangle_dev@outlook.com>
2025-06-28 20:46:29 +0800

    chore(docs): 更新文档标题为 'Dify Chat 文档' 并新增服务器端口配置

--- packages/docs/rspress.config.ts
+++ packages/docs/rspress.config.ts
@@ -3,11 +3,17 @@ import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
 	root: path.join(__dirname, 'docs'),
-	title: 'My Site',
+	title: 'Dify Chat 文档',
 	icon: '/rspress-icon.png',
 	logo: {
 		light: '/rspress-light-logo.png',
 		dark: '/rspress-dark-logo.png',
+	},
+	builderConfig: {
+		server: {
+			// 指定启动端口
+			port: 6200,
+		},
 	},
 	themeConfig: {
 		socialLinks: [
