2392a3fdd83749158a15f26e4e46ede83f2fd731
lexmin0412 <zhangle_dev@outlook.com>
2025-07-03 22:59:19 +0800

    feat(components): 新增 MessageFileList 导出

--- packages/components/src/index.tsx
+++ packages/components/src/index.tsx
@@ -1,13 +1,14 @@
-import "./index.css";
+import './index.css'
 
-export { Button } from "./Button";
-export { Chatbox } from "./chatbox";
-export { MarkdownRenderer } from "./markdown-renderer";
-export { ConversationList } from "./conversation-list";
-export { MessageSender } from "./message-sender";
-export { default as AppIcon } from "./chatbox/app-icon";
-export { AppInfo } from "./chatbox/app-info";
-export { default as AppInputForm } from "./chatbox/app-input-form";
-export { type ILucideIconProps, default as LucideIcon } from "./lucide-icon";
-export { default as WorkflowLogs } from "./chatbox/message/workflow-logs";
-export * from "./layout";
+export { Button } from './Button'
+export { Chatbox } from './chatbox'
+export { MarkdownRenderer } from './markdown-renderer'
+export { ConversationList } from './conversation-list'
+export { MessageSender } from './message-sender'
+export { default as AppIcon } from './chatbox/app-icon'
+export { AppInfo } from './chatbox/app-info'
+export { default as AppInputForm } from './chatbox/app-input-form'
+export { type ILucideIconProps, default as LucideIcon } from './lucide-icon'
+export { default as WorkflowLogs } from './chatbox/message/workflow-logs'
+export { default as MessageFileList } from './chatbox/message/file-list'
+export * from './layout'
