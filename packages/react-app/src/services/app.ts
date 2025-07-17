a419eea415923bce0f350a49949edcfb5170e86a
lexmin0412 <zhangle_dev@outlook.com>
2025-07-17 22:59:22 +0800

    refactor(react-app): 简化应用服务定义

--- packages/react-app/src/services/app.ts
+++ packages/react-app/src/services/app.ts
@@ -3,8 +3,14 @@ import { uniqueId } from 'lodash-es'
 
 // 支持多种存储方式
 interface IAppStorageAdapter {
+	/**
+	 * 获取应用列表，如果是单应用模式，将会取第一个
+	 */
 	getApps(): Promise<IDifyAppItem[]>
-	getApp(id: string): Promise<IDifyAppItem>
+	/**
+	 * 通过 ID 获取应用，适用于多应用
+	 */
+	getAppByID(id: string): Promise<IDifyAppItem>
 }
 
 // 固定的应用列表
@@ -23,17 +29,15 @@ const appList = [
 	} as IDifyAppItem,
 ]
 
-class StaticDataAdapter implements IAppStorageAdapter {
+class AppService implements IAppStorageAdapter {
 	async getApps(): Promise<IDifyAppItem[]> {
 		return Promise.resolve(appList)
 	}
 
-	async getApp(id: string): Promise<IDifyAppItem> {
+	async getAppByID(id: string): Promise<IDifyAppItem> {
 		const appList = await this.getApps()
 		return Promise.resolve(appList.find(item => item.id === id) as IDifyAppItem)
 	}
 }
 
-const appService = new StaticDataAdapter()
-
-export default appService
+export default new AppService()
