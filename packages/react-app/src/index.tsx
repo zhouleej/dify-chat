4ea9b5bb1aae469904184683abb71dc99e265a00
lexmin0412 <zhangle_dev@outlook.com>
2025-08-10 11:49:13 +0800

    chore: 移除 rybbit 的引入和相关依赖，Close #302

--- packages/react-app/src/index.tsx
+++ packages/react-app/src/index.tsx
@@ -1,16 +1,9 @@
 import { legacyLogicalPropertiesTransformer, StyleProvider } from '@ant-design/cssinjs'
 import '@ant-design/v5-patch-for-react-19'
 import { ThemeContextProvider } from '@dify-chat/theme'
-import rybbit from '@rybbit/js'
 import ReactDOM from 'react-dom/client'
 
 import App from './App'
-
-// 初始化 rybbit SDK
-rybbit.init({
-	analyticsHost: 'https://app.rybbit.io/api',
-	siteId: '296',
-})
 
 const rootEl = document.getElementById('root')
 if (rootEl) {
