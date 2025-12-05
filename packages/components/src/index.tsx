10261b26e82f0e74a36af75d287fc962ce2a9c06
lexmin0412 <zhangle_dev@outlook.com>
2025-12-05 13:28:56 +0800

    refactor(components): 移除子包中不再使用的组件

--- packages/components/src/index.tsx
+++ packages/components/src/index.tsx
@@ -1,9 +1,4 @@
 import './index.css'
 
-export { Button } from './Button'
-export { MarkdownRenderer } from './markdown-renderer'
-export { ConversationList } from './conversation-list'
-export { MessageSender } from './message-sender'
 export { type ILucideIconProps, default as LucideIcon } from './lucide-icon'
 export * from './layout'
-export { default as ParamsConfigEditor, type IParamItem } from './form/params-config-editor'
--- packages/components/src/markdown-renderer/utils.ts
+++
@@ -1,6 +0,0 @@
-import classnames from 'classnames'
-import { twMerge } from 'tailwind-merge'
-
-export const cn = (...cls: classnames.ArgumentArray) => {
-	return twMerge(classnames(cls))
-}
